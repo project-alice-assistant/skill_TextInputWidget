b13850395774bc3739ebb33db0958de142307ec2
Psychokiller1888 <laurentchervet@bluewin.ch>
2020-01-13 09:48:51 +0100

    Cleanup

--- widgets/TextInputWidget.py
+++ widgets/TextInputWidget.py
@@ -4,9 +4,9 @@ from core.base.model.Widget import Widget
 
 
 class TextInputWidget(Widget):
-
 	SIZE = 'w_medium_wide'
 	OPTIONS: dict = dict()
+
 
 	def __init__(self, data: sqlite3.Row):
 		super().__init__(data)

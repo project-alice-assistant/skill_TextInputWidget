3466cc6fa14a6d239d02207f36a8b4847507d05f
Psycho <laurentchervet@bluewin.ch>
2020-04-22 09:01:12 +0200

    update

--- widgets/SimpleCommand.py
+++ widgets/SimpleCommand.py
@@ -1,12 +1,12 @@
 import sqlite3
 
 from core.base.model.Widget import Widget
-from core.base.model.widgetSizes import WidgetSizes
+from core.base.model.WidgetSizes import WidgetSizes
 
 
 class SimpleCommand(Widget):
-	SIZE = WidgetSizes.w_wide
-	OPTIONS: dict = dict()
+	DEFAULT_SIZE = WidgetSizes.w_wide
+	DEFAULT_OPTIONS: dict = dict()
 
 
 	def __init__(self, data: sqlite3.Row):

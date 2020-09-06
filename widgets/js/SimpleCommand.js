e73bd5e57c069cc3c1bc5699f2f4ab803a838f7d
Psycho <laurentchervet@bluewin.ch>
2020-09-06 16:47:48 +0200

    Remember me function

--- widgets/js/SimpleCommand.js
+++ widgets/js/SimpleCommand.js
@@ -1,5 +1,6 @@
 $(function () {
 	let savedToken = '';
+	let remember = false;
 	let topics = [
 		'hermes/dialogueManager/sessionEnded'
 	]
@@ -24,8 +25,11 @@ $(function () {
 			savedToken = json['apiToken'];
 			if(!savedToken){
 				alert(json['message']);
+				unsetCookie('SimpleCommand_username')
+				unsetCookie('SimpleCommand_pin')
 				return;
 			}
+
 			$('#TextInputWidget_login').hide();
 			$('#TextInputWidget_query').show()
 		});
@@ -33,8 +37,8 @@ $(function () {
 	}
 
 	function process(token, siteID, qry, sessionId) {
-		let url = "";
-		if( sessionId == "" ){
+		let url = '';
+		if(sessionId == ''){
 			url = '/api/v1.0.1/dialog/process/';
 		} else {
 			console.log(sessionId)
@@ -59,12 +63,18 @@ $(function () {
 		};
 
 		$.ajax(settings).done(function (response) {
-			$('#sessionId').val(response['sessionId']);
+		 	$('#sessionId').val(response['sessionId']);
 		});
 	}
 
 	$('#login').on('click', function () {
-		login($('#username').val(), $('#pin').val());
+		let username = $('#username').val().trim();
+		let pin = $('#pin').val().trim();
+		if (remember) {
+			document.cookie = 'SimpleCommand_username=' + username;
+			document.cookie = 'SimpleCommand_pin=' + pin;
+		}
+		login(username, pin);
 	});
 
 	$('#logout').on('click touchstart', function () {
@@ -78,13 +88,13 @@ $(function () {
 	});
 
 	$('#qry').on('keydown', function(e) {
-		if (e.key == "Enter") {
+		if (e.key == 'Enter') {
 			process(savedToken, $('#siteID').val(), $('#qry').val(), $('#sessionId').val());
 		}
 	});
 
 	$('#pin').on('keydown', function(e) {
-		if (e.key == "Enter") {
+		if (e.key == 'Enter') {
 			login($('#username').val(), $('#pin').val());
 		}
 	});
@@ -108,10 +118,20 @@ $(function () {
 		}
 	}
 
+	$('#rememberMe').on('click touchstart', function() {
+		remember = !!$(this).is(':checked');
+
+		if (!remember) {
+			document.cookie = 'apiToken=; expires=Thu, 01 Jan 1970 00:00:00 UTC;';
+		}
+	});
+
 	mqttRegisterSelf(onConnect, 'onConnect');
 	mqttRegisterSelf(onMessage, 'onMessage');
 
-	if( $('#username').val() != '' && $('#pin').val() != ''){
-		login($('#username').val(), $('#pin').val());
+	let c_username = getCookie('SimpleCommand_username');
+	let c_pin = getCookie('SimpleCommand_pin');
+	if (c_username != '' && c_pin != '') {
+		login(c_username, c_pin);
 	}
 });
